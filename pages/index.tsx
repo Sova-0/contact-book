7489730e1ef1ae72ed53d643e5f1b8650e4268dc
Sova <antongoraynov0@gmail.com>
2025-11-06 08:48:46 +0300

    удаление ненужных функций

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Container } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from '../store/index';
-import { addContact, setFilterType, setSortOrder, selectFilteredContacts, ContactType } from "@/features/contacts/contactsSlice";
+import { setFilterType, setSortOrder, ContactType } from "@/features/contacts/contactsSlice";
 import { useState } from "react";
 import { ContactFormModal } from '@/components/ContactFormModal';
 import { ContactsList } from "@/components/ContactList";
@@ -8,7 +8,6 @@ import { ContactsFilter } from "@/components/FilterPanel";
 
 export default function Home() {
   const dispatch = useAppDispatch();
-  const contacts = useAppSelector(selectFilteredContacts);
   const filterType = useAppSelector(state => state.contacts.filterType);
   const sortOrder = useAppSelector(state => state.contacts.sortOrder);
   
@@ -17,9 +16,6 @@ export default function Home() {
   const handleOpen = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
 
-  const handleAdd = () => {
-    dispatch(addContact({ type: 'email', value: 'test@email.com', description: 'testDescription' }))
-  }
   return (
     <Container className="py-4">
       <div className="d-flex justify-content-between align-items-center mb-3">
